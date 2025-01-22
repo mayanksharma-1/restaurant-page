a347db639b214b4aa97085362640c04e8f09112e
Mayank <76253494+mayanksharma-1@users.noreply.github.com>
2025-01-22 14:17:50 +0000

    Add event listeners to navigation buttons to update content based on button ID

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert(`this is working!`);
+let buttons = document.getElementsByClassName('navBtn');
+let content = document.getElementById('content');
+const mainContent = {
+    home: `<p> Welcome to my home page </p>`,
+    menu: `<p> Welcome to my menu page </p>`,
+    about: `<p> Welcome to my about page </p>`,
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (e) => {
+        console.log(e.target.id);
+        console.log(mainContent[e.target.id]);
+        content.innerHTML = mainContent[e.target.id];
+        
+    });
+}
