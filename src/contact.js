820d9aca4092812b4a534fbe48e9146b187e6439
Mayank Sharma <as.mayanksharma@gmail.com>
2025-01-23 16:16:22 +0530

    create contact  page module

---
+++ src/contact.js
@@ -0,0 +1,24 @@
+function createContact() {
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
+
+    const phoneNumber = document.createElement('p');
+    phoneNumber.textContent = '📞 123-456-789';
+
+    const address = document.createElement('p');
+    address.textContent = '🏠 123 Some Street, SomePlace, India';
+
+    // const restaurantLocation = document.createElement('img');
+    // restaurantLocation.src = 'images/location.png';
+    // restaurantLocation.alt = 'Restaurant Location';
+
+    contact.appendChild(phoneNumber);
+    contact.appendChild(address);
+    contact.appendChild(restaurantLocation);
+
+    return contact;
+}
+
+
+
+export default createContact;
